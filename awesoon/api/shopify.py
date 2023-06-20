3667e6bc1206bc763ba3f425f1c4345d41ab5d4f
farid <farid@korbit.ai>
2023-06-20 19:55:54 -0400

    query-toget-shopify-orders

--- awesoon/api/shopify.py
+++ awesoon/api/shopify.py
@@ -1,10 +1,11 @@
 
-from awesoon.core.shop import get_shop_categories, get_shop_policies, get_shop_products, shop_compute
+from awesoon.core.shop import get_shop_categories, get_shop_orders, get_shop_policies, get_shop_products
 from flask_restx import Namespace, Resource
 
 
 ns = Namespace(
-    "shopify-query", "This namespace is resposible for shop related data generation")
+    "shopify-query", "This namespace is resposible for shop related data generation"
+)
 
 
 shopify_parser = ns.parser()
@@ -43,5 +44,17 @@ class ShopGetCategories(Resource):
         categories = get_shop_categories(id, args["app_name"])
         result = {
             "categories": categories
+        }
+        return result
+
+
+@ns.route("/<id>/orders")
+class ShopOrders(Resource):
+    @ns.expect(shopify_parser)
+    def get(self, id):
+        args = shopify_parser.parse_args()
+        orders = get_shop_orders(id, args["app_name"])
+        result = {
+            "orders": orders
         }
         return result
