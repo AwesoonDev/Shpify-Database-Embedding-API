fa41b1a63887ea8631bd29c1d4672bc1a1ce5c63
farid <farid@korbit.ai>
2023-06-26 22:34:20 -0400

    use config for redis variables

--- awesoon/celery/tasks.py
+++ awesoon/celery/tasks.py
@@ -1,15 +1,17 @@
 from celery import Celery
 
 from awesoon.core.docs import scan_shop
-
+from awesoon.config import config
 # configure celery app with Redis as the message broker
-app = Celery('scan_tasks', broker='redis://localhost:6379/0', result_backend='redis://localhost:6379/0')
+app = Celery("scan_tasks",
+             broker=f"{config.celery_broker.url}/0",
+             result_backend=f"{config.celery_broker.url}/0")
 
 
 @app.task
 def manual_scan_request(shop_id, scan_id, args):
     scan_shop(shop_id, scan_id, args)
-    print(f'Scan complete: {scan_id}')
+    print(f"Scan complete: {scan_id}")
     return True
 
 
